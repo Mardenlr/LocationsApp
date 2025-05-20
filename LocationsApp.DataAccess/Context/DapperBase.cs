11f3ae70ad0108098db0bf842a31c173a72e2183
Marden Rodrigues <marden.dba@gmail.com>
2025-05-19 23:37:41 -0300

    Updated to .NET 9.0

--- LocationsApp.DataAccess/Context/DapperBase.cs
+++ LocationsApp.DataAccess/Context/DapperBase.cs
@@ -1,8 +1,8 @@
 ﻿using Dapper;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using System.Collections.Generic;
 using System.Data;
-using System.Data.SqlClient;
 using System.Threading.Tasks;
 
 namespace LocationsApp.DataAccess.Context
